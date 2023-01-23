ce9fd6e1556408275fb335ff057d6cde36783c46
Iva <IvRuskova@gmail.com>
2023-01-23 23:31:23 +0200

    Update GuessANumber.cs
    Add restart method

--- code/GuessANumber.cs
+++ code/GuessANumber.cs
@@ -14,26 +14,27 @@ public class GuessANumber
 			Console.Write("Guess a number (1-100): ");
 			string playerInput = Console.ReadLine();
 		bool isValid = int.TryParse(playerInput, out int playerNumber);
-		if (isValid)
-		{
-			if (playerNumber == compurterNumber)
+			if (isValid)
 			{
-				Console.WriteLine("You guessed it!");
-				break;
-			}
-			else if (playerNumber>compurterNumber)
-			{
-				Console.WriteLine("Too High");
-			}
-			else if (playerNumber<compurterNumber)
-			{
-				Console.WriteLine("Too Low");
-			}
-			else
-			{
-				Console.WriteLine("Invalid input. ");
+				if (playerNumber == compurterNumber)
+				{
+					Console.WriteLine("You guessed it!");
+					break;
+				}
+				else if (playerNumber>compurterNumber)
+				{
+					Console.WriteLine("Too High");
+				}
+				else if (playerNumber<compurterNumber)
+				{
+					Console.WriteLine("Too Low");
+				}
+				else
+				{
+					Console.WriteLine("Invalid input. ");
+				}
 			}
 		}
-		}
-	}
+		Application.Restart();	
+}
 }
