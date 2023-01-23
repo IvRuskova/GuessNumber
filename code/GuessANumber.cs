d526a133d0837d036db0524c146f57f112effb3a
Iva <IvRuskova@gmail.com>
2023-01-23 23:19:15 +0200

    Create GuessANumber.cs
    Add class GuessANumber

---
+++ code/GuessANumber.cs
@@ -0,0 +1,39 @@
+﻿
+
+public class GuessANumber
+{
+	using System;
+	using System.Numeric;
+	public static void Main(string[] args)
+	{
+		Random random = new Random();
+		int compurterNumber = randomNumber.Next(1, 101);
+
+		while (true)
+		{
+			Console.Write("Guess a number (1-100): ");
+			string playerInput = Console.ReadLine();
+		bool isValid = int.TryParse(playerInput, out int playerNumber);
+		if (isValid)
+		{
+			if (playerNumber == compurterNumber)
+			{
+				Console.WriteLine("You guessed it!");
+				break;
+			}
+			else if (playerNumber>compurterNumber)
+			{
+				Console.WriteLine("Too High");
+			}
+			else if (playerNumber<compurterNumber)
+			{
+				Console.WriteLine("Too Low");
+			}
+			else
+			{
+				Console.WriteLine("Invalid input. ");
+			}
+		}
+		}
+	}
+}
